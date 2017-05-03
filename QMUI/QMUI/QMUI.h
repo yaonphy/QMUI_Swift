f2f2382770c778f0e52a16eac6e775fb3125b011
yaongfeng <yaongfeng@gmail.com>
2017-05-03 23:21:35 +0800

    initialize

---
+++ QMUI/QMUI/QMUI.h
@@ -0,0 +1,19 @@
+//
+//  QMUI.h
+//  QMUI
+//
+//  Created by yaongfeng on 2017/5/3.
+//  Copyright © 2017年 yaongfeng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for QMUI.
+FOUNDATION_EXPORT double QMUIVersionNumber;
+
+//! Project version string for QMUI.
+FOUNDATION_EXPORT const unsigned char QMUIVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <QMUI/PublicHeader.h>
+
+
